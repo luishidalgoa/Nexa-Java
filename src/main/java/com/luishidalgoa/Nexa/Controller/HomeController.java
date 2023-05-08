9cbcedce5ee6e277eb099b07170a83d110c85329
Luis hidalgo aguilar <luishidalgoa@outlook.es>
2023-05-08 21:42:27 +0200

    Documentado y refactorizado

--- src/main/java/com/luishidalgoa/Nexa/Controller/HomeController.java
+++ src/main/java/com/luishidalgoa/Nexa/Controller/HomeController.java
@@ -5,7 +5,6 @@ import com.luishidalgoa.Nexa.Model.DAO.*;
 import com.luishidalgoa.Nexa.Model.DAO.Publications.PublicationDAO;
 import com.luishidalgoa.Nexa.Model.DTO.*;
 import com.luishidalgoa.Nexa.Model.Domain.Publications.Publication;
-import com.luishidalgoa.Nexa.Model.Enum.Language;
 import com.luishidalgoa.Nexa.Thread.ThreadUpdatePublications;
 import com.luishidalgoa.Nexa.Interfaces.iControllers.iControllerHome;
 import com.luishidalgoa.Nexa.Utils.*;
@@ -21,10 +20,8 @@ import javafx.scene.layout.VBox;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.ResourceBundle;
-import java.util.Set;
+import java.util.*;
+import java.util.logging.Level;
 
 public class HomeController implements Initializable, iControllerHome  {
     @FXML
@@ -56,6 +53,7 @@ public class HomeController implements Initializable, iControllerHome  {
     //-------------------------------
     private UserDTO user_logged;
     PublicationDAO publicationDAO = PublicationDAO.getInstance();
+    private final static java.util.logging.Logger logger= com.luishidalgoa.Nexa.Utils.Logger.CreateLogger("com.luisidalgoa.com.Controller.HomeController");
 
     public HomeController() {
     }
@@ -80,7 +78,7 @@ public class HomeController implements Initializable, iControllerHome  {
      */
     public void suggestion_panel() {
         Set<UserDTO> users = getRecommendUsers();
-        if (users != null) {
+        if (!users.isEmpty()) {
             for (UserDTO aux : users) {
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("userPanel.fxml"));
                 try {
@@ -90,9 +88,12 @@ public class HomeController implements Initializable, iControllerHome  {
                     userPanelController.setData(aux);
                     suggestion_panel.getChildren().add(card);
                 } catch (IOException e) {
+                    logger.log(Level.SEVERE, e.getMessage());
                     throw new RuntimeException(e);
                 }
             }
+        }else{
+            logger.log(Level.WARNING,"WARNING. the list RecommendUsers of the function: getRecommendUsers() is empty");
         }
     }
 
@@ -102,11 +103,12 @@ public class HomeController implements Initializable, iControllerHome  {
      * de nuestro HomeController
      */
     public void updatePublicationPanel() {
-        for (int i = vBox_publications.getChildren().size() - 1; i >= 2; i--) {
-            vBox_publications.getChildren().remove(i);
+        if (vBox_publications.getChildren().size() > 2) {
+//limpiaremos toda la vista de una porccion de nuestro vbox_publication. Porque queremos preservar los 2 elementos principales
+            vBox_publications.getChildren().subList(2, vBox_publications.getChildren().size()).clear();
         }
         Set<PublicationDTO> publications = getAllPublications();
-        if (publications != null) {
+        if (!publications.isEmpty()) {
             for (PublicationDTO aux : publications) {
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("publication.fxml"));
                 try {
@@ -116,9 +118,12 @@ public class HomeController implements Initializable, iControllerHome  {
                     publicationController.setData(user_logged, aux);
                     vBox_publications.getChildren().add(card);
                 } catch (IOException e) {
+                    logger.log(Level.SEVERE,e.getMessage());
                     throw new RuntimeException(e);
                 }
             }
+        }else{
+            logger.log(Level.WARNING,"WARNING. the list Publications of the function: getAllPublications() is empty");
         }
     }
 
@@ -132,9 +137,11 @@ public class HomeController implements Initializable, iControllerHome  {
                 PublicationDAO.getInstance().save(new Publication(textField_post.getText(), user_logged));
                 Execute.mainController.updatePublicationPanel();
             } else {
-                label_message_post.setText("Error. The Publication hasn´t been saved.");
+                logger.log(Level.WARNING,"WARNING. The Publication hasn´t been saved. its very short");
+                label_message_post.setText("The Publication is very Short");
             }
         } catch (SQLException e) {
+            logger.log(Level.SEVERE,e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -150,13 +157,14 @@ public class HomeController implements Initializable, iControllerHome  {
             publications=Utils.orderByTime(publications);
             return publications;
         } catch (SQLException e) {
+            logger.log(Level.SEVERE,e.getMessage());
             throw new RuntimeException(e);
         }
     }
     /**
      * El proposito de este metodo es extraer todos los usuarios de la bbdd
      * Este metodo esta pensado para ser utilizado en el metodo Suggestion_Panel()
-     * @return
+     * @return dd
      */
     private Set<UserDTO> getRecommendUsers() {
         try {
@@ -171,6 +179,7 @@ public class HomeController implements Initializable, iControllerHome  {
             }
             return result;
         } catch (SQLException e) {
+            logger.log(Level.SEVERE,e.getMessage());
             throw new RuntimeException(e);
         }
     }
@@ -180,21 +189,22 @@ public class HomeController implements Initializable, iControllerHome  {
 
     /**
      * Actuala los elementos del fxml asignandole el texto en el idioma correspondiente
-     * @throws SQLException
      */
     public void UpdateLanguage() {
         try {
             //Buscamos el idioma en la configuracion del usuario
-            Language language=User_optionsDAO.get_instance().FindLanguage(user_logged.getUser_name()).getLanguage();
-            Home_Home.setText(Translated.get_instance().getTraslated().map.get(language.name()).map.get("Home_Home")); //buscamos la clave del objeto traducido en el mapa del idioma
-            Home_Collections.setText(Translated.get_instance().getTraslated().map.get(language.name()).map.get("Home_Collections"));
-            Home_Message.setText(Translated.get_instance().getTraslated().map.get(language.name()).map.get("Home_Message"));
-            Home_Perfil.setText(Translated.get_instance().getTraslated().map.get(language.name()).map.get("Home_Perfil"));
-            Home_Suggestions.setText(Translated.get_instance().getTraslated().map.get(language.name()).map.get("Home_Suggestions"));
-            btn_post.setText(Translated.get_instance().getTraslated().map.get(language.name()).map.get("Home_Post"));
-            Home_ShowMore.setText(Translated.get_instance().getTraslated().map.get(language.name()).map.get("Home_ShowMore"));
+            String language= Objects.requireNonNull(User_optionsDAO.get_instance().FindLanguage(user_logged.getUser_name())).getLanguage().name();
+            HashMapSerializable<String,String>translated= Translated.get_instance().getTraslated().map.get(language);
+            Home_Home.setText(translated.map.get("Home_Home")); //buscamos la clave del objeto traducido en el mapa del idioma
+            Home_Collections.setText(translated.map.get("Home_Collections"));
+            Home_Message.setText(translated.map.get("Home_Message"));
+            Home_Perfil.setText(translated.map.get("Home_Perfil"));
+            Home_Suggestions.setText(translated.map.get("Home_Suggestions"));
+            btn_post.setText(translated.map.get("Home_Post"));
+            Home_ShowMore.setText(translated.map.get("Home_ShowMore"));
             updatePublicationPanel();
         } catch (SQLException e) {
+            logger.log(Level.SEVERE,e.getMessage());
             throw new RuntimeException(e);
         }
     }
